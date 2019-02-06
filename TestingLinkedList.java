24f3bbde0b97313ccfc578e8b5a62f3545a39f02
jessica-cruz-dev <jessica.cruz.dev@gmail.com>
2019-02-06 13:41:35 -0600

    Deleted driver function

--- TestingLinkedList.java
+++ TestingLinkedList.java
@@ -1,47 +1,49 @@
+package assignment_1;
+
 /*
 * Write test cases to test your implementation of the singly-linked list and its methods, and discuss
 * the effectiveness of your test cases in terms of their pros and cons.
 */
-package assignment_1;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 public class TestWorker {
 
- private LinkedList list = new LinkedList();
+ private LinkedList list1 = new LinkedList();
+ private LinkedList list2 = new LinkedList();
 
  @Test
  @DisplayName("add() Method Test")
- public void testWorkMethod()
+ public void testAddMethod()
  {
-   assertEquals(list.add(),"I'm working already!", "Mismatch between test text and method text" );
+   assertEquals(list1.add(),"I'm working already!", "Mismatch between test text and method text" );
  }
 
 
  @Test
  @DisplayName("remove() Method Test")
- public void newtestJCWorkerFail()
+ public void testRemoveMethod()
  {
 
-   assertTrue(list.remove() == "I'm taking a self-care day!", "Test failed - Mismatch between test text and method text");
+   assertTrue(list1.remove() == "I'm taking a self-care day!", "Test failed - Mismatch between test text and method text");
  }
 
 @Test
  @DisplayName("size() Method Test")
- public void newtestJCWorkerPass()
+ public void testSizeMethod()
  {
 
-   assertNotEquals(list.size() == "I'm taking a 2 hour lunch brb", "Test failed - Mismatch between test text and method text");
+   assertNotEquals(list1.size() == "I'm taking a 2 hour lunch brb", "Test failed - Mismatch between test text and method text");
  }
 
 
 @Test
 @DisplayName("equals() Method Test")
-public void newtestJCWorkerPass()
+public void testEqualsMethod()
 {
 
-  assertNotEquals(list.equals() == "I'm taking a 2 hour lunch brb", "Test failed - Mismatch between test text and method text");
+  assertNotEquals(list1.equals() == "I'm taking a 2 hour lunch brb", "Test failed - Mismatch between test text and method text");
 }
 
 
